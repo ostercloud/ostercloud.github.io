fca6dcec14133003d428e75596ad1e896294a10a
Allen Oster <osterallen@gmail.com>
2020-10-27 13:43:45 -0500

    Upload latest version

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //Get the Paramenters fro the URL
 function getQueryStringValue(queryParam){
     // Get current URL.
@@ -17,16 +16,29 @@ function getQueryStringValue(queryParam){
      return undefined;
    }
  }
- 
+ var url = window.location.href
+ var APIKey = '';
+ if (getQueryStringValue('key')) {
+   console.log('API Key Found: OK');
+   var APIKey = getQueryStringValue('key')
+ } else {
+   var APIKey = prompt('You will need an API key from ( https://forebears.io/onograph ) You can register for a free account and then login to see your API Key, then enter it below. Please enter your API Key:');
+   url += '?key=' + APIKey;
+   window.location.href = url;
+ };
+
  // If currentURL="/index.html?search=test" then,
  // getQueryStringValue("search") returns "test"
  // getQueryStringValue("notPresentQueryParam") returns undefined
 
 //Set variables that are needed in the Vue Template
-var searchname = (typeof getQueryStringValue("search") === 'undefined') ? '' : getQueryStringValue("search");
+//var searchname = (typeof getQueryStringValue("search") === 'undefined') ? '' : getQueryStringValue("search");
+var searchfn = (typeof getQueryStringValue("fn") === 'undefined') ? '' : getQueryStringValue("fn");
+var searchln = (typeof getQueryStringValue("ln") === 'undefined') ? '' : getQueryStringValue("ln");
 
 //Name API URL
-var nameApi = 'https://ono.4b.rs/v1/jur?key=3fb097ed41c846debeab3789a613ccc3&name='+ searchname +'&type=surname';
+var nameApi = 'https://ono.4b.rs/v1/nat?key=' + APIKey + '&fn='+ searchfn +'&sn='+ searchln;
+console.log(nameApi); 
 
 //grab HTML Countries Table and create JSON from it
 var arbtbl = $('#arbtbl').tableToJSON();
@@ -44,11 +56,11 @@ function search(item1, item2, myArray){
         result++
   } }
     if (result === 2) {
-      languageArab = 'Very Likely';
+      languageArab = 'Very Likely (2 Matches)';
     } else if (result === 1) {
-      languageArab = 'Possibly';
+      languageArab = 'Possibly (1 Match)';
     } else {
-      languageArab = 'Not Likely'
+      languageArab = 'Not Likely (No Matches)'
     }
     return languageArab;
 };
@@ -59,22 +71,29 @@ function search(item1, item2, myArray){
     el: '#app',
     data: {
         //If URI includes ?search= then display the search parameter else display default text. 
-        query: (typeof getQueryStringValue("search") === 'undefined') ? 'Search for a Name to Begin' : 'You searched for ' + getQueryStringValue("search"),
+        query: (typeof getQueryStringValue("ln") === 'undefined') ? 'Search for a Name to Begin' : 'You searched for ' + searchfn + ' '+ searchln,
+        forebears: 'https://forebears.io/surnames/' + searchln,
         country1: 'none',
         country2: 'none ',
         arabspeaking: '',
+        apikey: APIKey,
     },
     watch: {
         country1: function(NewValue, OldValue) {
            //check if the country is arabic speaking by crossreferencing our arabic speaking countries table.
            this.arabspeaking = search(this.country1.jurisdiction, this.country2.jurisdiction, arbtbl)
+          },
+        country2: function(NewValue, OldValue) {
+          //check if the country is arabic speaking by crossreferencing our arabic speaking countries table.
+          this.arabspeaking = search(this.country1.jurisdiction, this.country2.jurisdiction, arbtbl)
           }
     },
     mounted() {
       //fetch forebears.io info and set the top two countries to data variables. 
       axios.get(nameApi).then(response => {
-        this.country1 = response.data.jurisdictions[0];
-        this.country2 = response.data.jurisdictions[1];
+        console.log(response);
+        this.country1 = response.data.countries[0];
+        this.country2 = response.data.countries[1];
       })
     }
     })
